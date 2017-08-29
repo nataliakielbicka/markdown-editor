0f150ed77afefee386adc1fa44d6734efec23482
Natalia <natalia.kielbicka@o2.pl>
2017-08-29 22:42:13 +0200

    make some variables changes - make convert func more global

--- js/app.js
+++ js/app.js
@@ -1,20 +1,18 @@
 (function() {
     "use strict";
 
-    var input = document.getElementById("input");
     var output = document.getElementById("output");
     //var convert = document.getElementById("convert");
 
     function convertH1() {
-        var h1Text = input.value;
-        var h1TextLen = h1Text.length;
-        var h1PureText = h1Text.substring(1, h1TextLen);
-        output.innerHTML = "<h1>" + h1PureText + "</h1>";
+        var inputVal = document.getElementById("input").value;
+        var inputValLen = inputVal.length;
+        output.innerHTML = "<h1>" + inputVal.substring(1, inputValLen) + "</h1>";
     }
     var timer = 0;
     document.body.addEventListener("keydown", function() {
         clearTimeout(timer);
         timer = setTimeout(convertH1, 1500);
     });
-    //console.log(input.textContent.substring(1, input.textContent.length))
+    //console.log(input.textContent.substring(1, input.textContent.length));
 })();
