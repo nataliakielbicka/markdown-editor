9552085cb21f08740b39e72ff6574d42fdd77dc8
Natalia <natalia.kielbicka@o2.pl>
2017-08-29 22:10:29 +0200

    convert #input to textarea

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@
     var convert = document.getElementById("convert");
 
     function convertH1() {
-        var h1Text = input.textContent;
+        var h1Text = input.value;
         var h1TextLen = h1Text.length;
         var h1PureText = h1Text.substring(1, h1TextLen);
         output.innerHTML = "<h1>" + h1PureText + "</h1>";
