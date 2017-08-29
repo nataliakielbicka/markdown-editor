2e3b9fea5cdd7725c86d4752c4e00839f538cba2
Natalia <natalia.kielbicka@o2.pl>
2017-08-29 22:01:39 +0200

    add function to convert h1 markup

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
 (function() {
     "use strict";
 
+    var input = document.getElementById("input");
+    var output = document.getElementById("output");
+    var convert = document.getElementById("convert");
 
+    function convertH1() {
+        var h1Text = input.textContent;
+        var h1TextLen = h1Text.length;
+        var h1PureText = h1Text.substring(1, h1TextLen);
+        output.innerHTML = "<h1>" + h1PureText + "</h1>";
+    }
+
+    convert.addEventListener("click", function() {
+        convertH1();
+    });
+    //console.log(input.textContent.substring(1, input.textContent.length))
 })();
