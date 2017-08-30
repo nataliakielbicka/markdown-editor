6bc2ffa7f1ae22c8d2ccc53ac4938f8ff6c6242b
Natalia <natalia.kielbicka@o2.pl>
2017-08-30 09:23:04 +0200

    make addElement function more reusable

--- js/app.js
+++ js/app.js
@@ -4,21 +4,21 @@
     var output = document.getElementById("output");
     var timer = 0;
 
-    function addElement() {
+    function addElement(node) {
         var inputVal = document.getElementById("input").value;
         var inputValArr = inputVal.split("\n");
         var inputValArrLen = inputValArr.length;
         var inputValArrLast = inputValArr[inputValArrLen - 1];
         var inputValArrLastLen = inputValArrLast.length;
         var inputAdd = inputValArrLast.substring(1, inputValArrLastLen);
-        var newDiv = document.createElement("h1");
+        var newDiv = document.createElement(node);
         var newContent = document.createTextNode(inputAdd);
         newDiv.appendChild(newContent);
         output.insertBefore(newDiv, output.nextElementSibling);
     }
 
     function convertH1() {
-        addElement();
+        addElement("h4");
     }
 
     document.body.addEventListener("keydown", function() {
